8c6fe65b5a4d79a56276ac9d13683ae57fe1dab1
kadensotek <bobby23jones@gmail.com>
2013-02-23 15:10:06 -0500

    Removed debugging lines

--- Player.java
+++ Player.java
@@ -4,7 +4,6 @@ public class Player extends Entity
 {
     public Player()
     {
-        //super(name);
         Random randGen = new Random();
         int maxHealth = 20;
         int defense = 0;
@@ -19,28 +18,20 @@ public class Player extends Entity
             if(rand == 0)
             {
                 maxHealth++;
-                System.out.printf("maxHealth = %d\n", maxHealth);
             }
             else if(rand == 1)
             {
                 defense++;
-                System.out.printf("defense = %d\n", defense);
             }
             else
             {
                 damageIncrement = (randGen.nextDouble() * 2.2);
                 damage += damageIncrement;
-
-                System.out.printf("damageIncrement = %f\n", damageIncrement);
             }
         }
 
-        System.out.printf("damage = %f\n", damage);
-
         damage = Math.round(damage * 4);
-        System.out.printf("Rounded damage = %f\n", damage);
         damage /= 4;
-        System.out.printf("Divided damage = %f\n", damage);
 
         setMaxHealth(maxHealth);
         setDefense(defense);
