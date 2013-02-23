6d320aac4f7f8e81ebcf2dcb493a20ab98b3d790
kadensotek <bobby23jones@gmail.com>
2013-02-23 13:00:17 -0500

    Created Item class; needs item options

---
+++ Item.java
@@ -0,0 +1,12 @@
+public class Item
+{
+    public Item(int itemID)
+    {
+        //TO DO: Assign items for itemID
+    }
+
+    public String getName()
+    {
+        //TO ADD: Item names
+    }
+}
