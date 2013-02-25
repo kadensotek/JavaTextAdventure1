f7b14cc2e38ec017c955b826cca0b49a7eb01bdb
kadensotek <bobby23jones@gmail.com>
2013-02-25 14:20:55 -0500

    changed input developing and created screen clear method

--- Game.java
+++ Game.java
@@ -18,14 +18,12 @@ public class Game
     {
         boolean quit = false;
         String userInput = "";
-        String userInputLowerCase = "";
 
         while(!quit)
         {
             userInput = getUserInput(userInput);
-            userInputLowerCase = userInput.toLowerCase();
 
-            quit = commandSwitch(userInputLowerCase, quit);
+            quit = commandProcessing(userInput, quit);
         }
     }
 
@@ -35,52 +33,59 @@ public class Game
         System.out.printf("\nEnter a command:\n");
         System.out.printf("> ");
 
-        userInput = scanner.next();
+        userInput = scanner.nextLine();
 
+        userInput = userInput.toLowerCase();         /* Makes string lower case */
         userInput = userInput.replaceAll("\\W", ""); /* Strips punctuation from string */
         System.out.println();
 
         return userInput;
     }
 
-    public boolean commandSwitch(String userInputLC, boolean quit)
-    /* Processes user commands with switch*/
+    public boolean commandProcessing(String userInput, boolean quit)
+    /* processes user commands */
     {
-         switch(userInputLC)
-         {
-             case "attack":
-             //              attack(player, monster);
-                           attack();
-                           break;
-             case "clear":
-                           clear();
-                           break;
-             case "go to": case "goto":
-                           go_to();
-                           break;
-             case "help":
-                           printHelp();
-                           break;
-             case "locations":
-                           locations();
-                           break;
-             case "monsters":
-                           monsters();
-                           //monsters(monsterList);
-                           break;
-             case "stats":
-                           stats(player);
-                           break;
-             case "quit":
-                           System.out.printf("Quitting.\n");
-                           quit = true;
-                           break;
-             default:
-                           System.out.printf("Default case.\n");
-                           break;
-        }
+       if(userInput.startsWith("attack"))
+       {
+           //attack(player, monster);
+           attack();
+       }
+       else if(userInput.startsWith("clear"))
+       {
+           clear();
+       }
+       else if(userInput.startsWith("go"))
+       {
+           go_to();
+       }
+       else if(userInput.startsWith("help"))
+       {
+           printHelp();
+       }
+       else if(userInput.startsWith("locations"))
+       {
+           locations();
+       }
+       else if(userInput.startsWith("monsters"))
+       {
+           monsters();
+           //monsters(monsterList);
+       }
+       else if(userInput.startsWith("stats"))
+       {
+           stats(player);
+       }
+       else if(userInput.startsWith("quit"))
+       {
+           System.out.printf("Quitting.\n");
+           quit = true;
+       }
+       else
+       { 
+           System.out.printf("Default case.\n");
+       }
 
-        return quit;
+       return quit;
     }
 
     public void initialize()
@@ -103,7 +108,10 @@ public class Game
 
     public void clear()
     {
-        System.out.printf("Clearing\n");
+        for(int i=0; i<50; i++)
+        {
+            System.out.println();
+        }
     }
 
     public void go_to()
@@ -113,15 +121,16 @@ public class Game
 
     public void printHelp()
     {
-        System.out.println("Commands");
-        System.out.println("##################################");
-        System.out.println("- Attack:\t\tAttack enemy.");
-        System.out.println("- Clear:\t\tClears the screen.");
-        System.out.println("- Go to <location>\tTakes you to <location>.");
-        System.out.println("- Locations:\t\tGives you a list of locations.");
-        System.out.println("- Monsters:\t\tTells you if any monsters are nearby.");
-        System.out.println("- Stats:\t\tShows your player stats.");
-        System.out.println("- Quit:\t\t\tExits the game.");
+        System.out.printf("\nCommands\n");
+        System.out.printf("-------------------------------------------------------------------\n");
+        System.out.printf("- Attack:\t\tAttack enemy.\n");
+        System.out.printf("- Clear:\t\tClears the screen.\n");
+        System.out.printf("- Go to <location>\tTakes you to <location>.\n");
+        System.out.printf("- Locations:\t\tGives you a list of locations.\n");
+        System.out.printf("- Monsters:\t\tTells you if any monsters are nearby.\n");
+        System.out.printf("- Stats:\t\tShows your player stats.\n");
+        System.out.printf("- Quit:\t\t\tExits the game.\n");
+        System.out.println();
     }
 
     public void locations()
@@ -137,12 +146,14 @@ public class Game
 
     public void stats(Player player)
     {
-        System.out.printf("\n--------------------------------------------------------------------\n\n");
+        System.out.printf("\nPlayer Stats\n");
+        System.out.printf("--------------------------------------------------------------------\n");
         System.out.printf("Name:\t%s\n", player.getName());
         System.out.printf("Gold:\t%d\n", player.getGold());
         System.out.printf("Health:\t%d/%d\n", player.getCurrentHealth(), player.getMaxHealth());
         System.out.printf("Damage:\t%.2f\n", player.getDamage());
         //System.out.printf("Location:\t%s\n", player.getLocation());
         //System.out.printf("Inventory:\n");
+        System.out.println();
     }
 }
