11ad283a49b41d8314fcb27a4a5873c72a94d58b
kadensotek <bobby23jones@gmail.com>
2013-02-25 19:52:57 -0500

    added a little extra code

--- Item.java
+++ Item.java
@@ -1,13 +1,18 @@
 public class Item
 {
+    private String itemName;
+    private int itemID;
+
     public Item(int itemID)
     {
         //TO DO: Assign items for itemID
+        this.itemID = itemID;
+        this.itemName = "defaultItem";
     }
 
     //public String getName()
-    public void getName()
+    public String getName()
     {
-        //TO ADD: Item names
+        return itemName;
     }
 }
