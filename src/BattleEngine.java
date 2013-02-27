b175d68a29a469075e59a214f2daf214b141b251
kadensotek <bobby23jones@gmail.com>
2013-02-27 16:23:03 -0500

    Added choice input and added structure

--- src/BattleEngine.java
+++ src/BattleEngine.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class BattleEngine
 {
@@ -18,9 +19,10 @@ public class BattleEngine
     private int mDefense;       /* Monster defense */
 
     private int damage;         /* Holds damage done by attack */ 
-    private int choice;         /* Holds player choice */
+    private int choice = 0;     /* Holds player choice */
+    private boolean valid;      /* Makes sure choice is in valid range */
     private int temp;           /* Holds misc. temp values */
-    private Random mLevelGen;   /* generates monster level */
+    private Random mLevelGen;   /* Generates monster level */
     private final int P_MAX_DAMAGE[] = {0,30,40,50,60,70,80,90,100,110,120,130,140,150,160}; /* max damages for player, based on level */
     private final int M_MAX_DAMAGE[] = {0,5,15,20,30,35,50,55,65,70,75,80,85};  /* max damages for monster, based on level */
 
@@ -42,6 +44,7 @@ public class BattleEngine
     }
 
     public void battleInit(Player player, Monster monster)
+    /* initializes battle stats */
     {
         int health;
 
@@ -73,27 +76,39 @@ public class BattleEngine
         mAttack = monster.getAttack();
         mDefense = monster.getDefense();
 
-        System.out.printf("\nYou have encountered a level %d %s!\n", mLevel, mName);
+        System.out.printf("\nYou have encountered a level %d %s!\n\n", mLevel, mName);
     }
 
     public void battleLoop()
+    /* main loop for battle */
     {
         while((mCurrentHealth > 0) && (pCurrentHealth > 0))
         {
+            printBattleHeader();
+
+            choice = getInputInt(1,4);  /* gets user int input with min and max valid values specified */
+
+
+            if(pCurrentHealth > 0)  mCurrentHealth-=20;
+            if(mCurrentHealth > 0)  pCurrentHealth-=20;
+        }
+    }
+
+    public void printBattleHeader()
+    /* Prints header for each turn */
+    {
             System.out.printf("%s health: %d/%d\n", pName, pCurrentHealth, pMaxHealth);
             System.out.printf("%s health: %d/%d\n", mName, mCurrentHealth, mMaxHealth);
             System.out.printf("What do you want to do?\n");
             System.out.printf("1. Attack\t2. Defend\n");
             System.out.printf("3. Item\t\t4. Flee\n");
- 
-            mCurrentHealth--;
-            pCurrentHealth--;
-        }
+            System.out.printf("> ");
     }
 
     public void battleEnd(Player player)
+    /* wraps battle up after player or monster is slain */
     {
-        if(mCurrentHealth==0)
+        if(mCurrentHealth<=0)
         {
             player.setCurrentHealth(pCurrentHealth);
             player.setExperience(10);
@@ -104,5 +119,54 @@ public class BattleEngine
             System.out.printf("\n%s has been slain.\n", pName);
             player.setAlive(false);
         }
+    }
+
+    public int getInputInt(int min, int max)
+    /* gets user input interger for choice; specifies min and max valid values */
+    {
+        Scanner scanner = new Scanner(System.in);
+        int ch = 0;
+        String input = "";
+
+        while(!input.matches("-?\\d+"))
+        {
+            input = scanner.nextLine();
+
+            if(!input.matches("-?\\d+"))
+            {
+                System.out.printf("Please select again:\n> ");
+            }
+            else
+            {
+                ch = Integer.parseInt(input);
+
+                valid = verifyChoice(ch, min, max);
+
+                if(!valid)   /* resets string if invalid choice is made */
+                {
+                    input = "";
+                    System.out.printf("Please select again:\n> ");
+                }
+            }
+        }
+
+        return ch;
+    }
+
+    public boolean verifyChoice(int ch, int min, int max)
+    /* verifies that chosen int is within specified range */
+    {
+        boolean validChoice = false;
+
+        if((ch >= min) && (ch <= max))
+        {
+            validChoice = true;
+        }
+        else
+        {
+            validChoice = false;
+        }
+
+        return validChoice;
     }
 }
