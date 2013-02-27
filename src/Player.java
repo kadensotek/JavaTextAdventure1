8dae9d8cdadbe1859b8f55a2e043dc532f2c21d1
kadensotek <bobby23jones@gmail.com>
2013-02-27 17:18:23 -0500

    Added level up mechanism

--- src/Player.java
+++ src/Player.java
@@ -44,4 +44,25 @@ public class Player extends Entity
         setAttack(attack);
         setLevel(1);
     }
+
+    public void levelUp()
+    {
+        Random randGen = new Random();
+        int attackGain = randGen.nextInt(10)+5;
+        int defenseGain = randGen.nextInt(10)+5;
+        int maxHealthGain = randGen.nextInt(25)+10;
+
+        setLevel(getLevel()+1);
+        setAttack(getAttack()+attackGain);
+        setDefense(getDefense()+defenseGain);
+        setMaxHealth(getMaxHealth()+maxHealthGain);
+        setCurrentHealth(getMaxHealth());
+
+        System.out.printf("\nYou reached level %d!\n", getLevel());
+        System.out.printf("Attack:  %.2f\n", getAttack());
+        System.out.printf("Defense: %d\n", getDefense());
+        System.out.printf("Health:  %d/%d\n", getCurrentHealth(), getMaxHealth());
+        System.out.println();
+         
+    }
 }
