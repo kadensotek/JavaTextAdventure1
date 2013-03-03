68e129b6cccd4a1adf749f00eeb957b7959b3d4c
kadensotek <bobby23jones@gmail.com>
2013-03-02 19:58:06 -0500

    Added potential things to look into

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,7 @@
 public class Location
 {
     // used as nodes on map
+    // links to other locations
+    // function to describe location
+    // items like chests and drawers (possibly create a container class like backpack?"
 }
