15b297d1329cb2b1ae3748b631781d205f65ad53
kadensotek <bobby23jones@gmail.com>
2013-03-03 19:01:49 -0500

    Added code for assigning paths

--- src/Location.java
+++ src/Location.java
@@ -5,23 +5,64 @@ public class Location
     // function to describe location
     // items like chests and drawers (possibly create a container class like backpack?"
 
-    private String name;
-    private String type;
-    private boolean northFlag;
+    private String name;    /* holds name of location */
+    private String type;    /* holds type of location (ex. swamp, forest, etc) */
+    //private boolean isLocked;  /* determines if location is open or not */
+    private boolean northFlag;  /* flags determine if path exists */
     private boolean eastFlag;
     private boolean southFlag;
     private boolean westFlag;
+    private Location northPath;  /* for potential paths */
+    private Location eastPath;
+    private Location southPath;
+    private Location westPath;
 
-    public Location(String name, String type)
+    public Location(String name, String type, boolean locked)
     {
         this.name = name;
         this.type = type;
+        this.isLocked = locked;
+
+        this.northFlag = false;
+        this.eastFlag = false;
+        this.southFlag = false;
+        this.westFlag = false;
+
+        this.northPath = null;
+        this.eastPath = null;
+        this.southPath = null;
+        this.westPath = null;
     }
 
-    public void addConnection()
+    public void addConnection(char direction, Location location)
     {
         //set boolean for direction
         //add connection in given direction
+
+        if(direction == 'n')
+        {
+            this.northPath = location;
+            this.northFlag = true;
+        }
+        else if(direction == 'e')
+        {
+            this.eastPath = location;
+            this.eastFlag = true;
+        }
+        else if(direction == 's')
+        {
+            this.southPath = location;
+            this.southFlag = true;
+        }
+        else if(direction == 'w')
+        {
+            this.westPath = location;
+            this.westFlag = true;
+        }
+        else
+        {
+
+        }
     }
 
     public void setNorthFlag(boolean northFlag)
