8e9aa5758ca4c778e6d757e5e583fdeb97da5187
kadensotek <bobby23jones@gmail.com>
2013-03-04 12:42:47 -0500

    Created test map for moving around

---
+++ src/MapTest.java
@@ -0,0 +1,48 @@
+public class MapTest
+{
+    Location one = new Location("1", "forest");
+    Location two = new Location("2", "swamp");
+    Location three = new Location("3", "town");
+    Location four = new Location("4", "forest");
+    Location five = new Location("5", "forest");
+    Location six = new Location("6", "town");
+    Location seven = new Location("7", "cave");
+    Location eight = new Location("8", "desert");
+    Location nine = new Location("9", "swamp");
+
+    public MapTest()
+    {
+        one.addConnection('e', two);
+        one.addConnection('s', four);
+
+        two.addConnection('e', three);
+        two.addConnection('s', five);
+        two.addConnection('w', one);
+
+        three.addConnection('s', six);
+        three.addConnection('w', two);
+
+        four.addConnection('n', one);
+        four.addConnection('e', five);
+        four.addConnection('s', seven);
+
+        five.addConnection('n', two);
+        five.addConnection('e', six);
+        five.addConnection('s', eight);
+        five.addConnection('w', four);
+
+        six.addConnection('n', three);
+        six.addConnection('s', nine);
+        six.addConnection('w', five);
+
+        seven.addConnection('n', four);
+        seven.addConnection('e', eight);
+
+        eight.addConnection('n', five);
+        eight.addConnection('e', nine);
+        eight.addConnection('w', seven);
+
+        nine.addConnection('n', six);
+        nine.addConnection('w', eight);
+    }
+}
