55dd2397d4c0a324ab7d84dc0dd9afb6c846347d
kadensotek <bobby23jones@gmail.com>
2013-03-04 21:03:48 -0500

    Fleshed out item names and details a little

--- src/Item.java
+++ src/Item.java
@@ -4,27 +4,56 @@ public class Item
     private int itemID;
     private boolean usable;
 
-/*
- *   1. Health potion
- *   2. Poison antidote
- *   3. Health increase
- *   4. Attack increase
- *   5. Defense increase
- *   6. Key
- *
- */
-
-
     public Item(int itemID)
     {
         //TO DO: Assign items for itemID
         this.itemID = itemID;
         this.itemName = "defaultItem";
+
+        if(itemID == 1)
+        {
+            this.itemName = "Health Potion";
+            this.usable = true;
+        }
+        else if(itemID == 2)
+        {
+            this.itemName = "Antidote";
+            this.usable = true;
+        }
+        else if(itemID == 3)
+        {
+            this.itemName = "Health Increase";
+            this.usable = true;
+        }
+        else if(itemID == 4)
+        {
+            this.itemName = "Attack Increase";
+            this.usable = true;
+        }
+        else if(itemID == 5)
+        {
+            this.itemName = "Defense Increase";
+            this.usable = true;
+        }
+        if(itemID == 6)
+        {
+            this.itemName = "Key";
+            this.usable = true;
+        }
     }
 
-    //public String getName()
+    public int getItemID()
+    {
+        return this.itemID;
+    }
+
     public String getName()
     {
-        return itemName;
+        return this.itemName;
+    }
+
+    public boolean getUsable()
+    {
+        return this.usable;
     }
 }
