270fdef9e396bf3aa72c3469c3233b2014712a3d
kadensotek <bobby23jones@gmail.com>
2013-03-10 23:26:00 -0400

    Created Container class and added basic code

---
+++ src/Container.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Container
+{
+    private boolean isEmpty;
+    private ArrayList<Item> contents = new ArrayList<Item>();
+
+    public Container()
+    {
+        this.isEmpty = true;
+    }
+
+    public boolean isEmpty()
+    {
+        return this.isEmpty;
+    }
+
+    //public Item getItem()
+
+    //public void addItem()
+}
