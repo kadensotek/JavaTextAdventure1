62a9e19ca03d37de863c89cac12169e3a370aef7
kadensotek <bobby23jones@gmail.com>
2013-03-12 20:28:16 -0400

    Monsters now spawn based on location type

--- src/MonsterCreator.java
+++ src/MonsterCreator.java
@@ -25,57 +25,169 @@ public class MonsterCreator
 
     private Monster GenerateMonster(Monster monster, Location currentLoc)
     {
-        System.out.printf("Current location is %s.\n", currentLoc.getName());
-
-        // pass location for monster creation eventually
         String name = "MissingNo";
-        int temp = randGen.nextInt(13);
+        String choice = "";
+        int max = 3;   /* sets default max of random generator */
+        int temp;      /* stores temp value for choosing monster */
 
-        if(temp==0)
+        if(currentLoc.getType().equals("forest"))
+        {
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "spider";
+            }
+            else if(temp==1)
+            {
+                choice = "wolf";
+            }
+            else
+            {
+                choice = "bandit";
+            }
+        }
+        else if(currentLoc.getType().equals("swamp"))
+        {
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "slime";
+            }
+            else if(temp==1)
+            {
+                choice = "zombie";
+            }
+            else
+            {
+                choice = "skeleton";
+            }
+        }
+        else if(currentLoc.getType().equals("town"))
+        {
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "rat";
+            }
+            else if(temp==1)
+            {
+                choice = "bandit";
+            }
+            else
+            {
+                choice = "spider";
+            }
+        }
+        else if(currentLoc.getType().equals("cave"))
+        {
+            max = 4;    /* four monsters to choose from here */
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "troll";
+            }
+            else if(temp==1)
+            {
+                choice = "bugbear";
+            }
+            else if(temp==2)
+            {
+                choice = "goblin";
+            }
+            else
+            {
+                choice = "bat";
+            }
+        }
+        else if(currentLoc.getType().equals("desert"))
+        {
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "nomad";
+            }
+            else if(temp==1)
+            {
+                choice = "spider";
+            }
+            else
+            {
+                choice = "giant";
+            }
+        }
+        else if(currentLoc.getType().equals("mountain"))
+        {
+            temp = randGen.nextInt(max);
+
+            if(temp==0)
+            {
+                choice = "troll";
+            }
+            else if(temp==1)
+            {
+                choice = "giant";
+            }
+            else
+            {
+                choice = "bandit";
+            }
+        }
+        else
+        {
+            System.out.printf("Invalid location.\n");
+        }
+
+
+        if(choice.equals("rat"))
         {
             monster.setName("Rat");
         }
-        else if(temp==1)
+        else if(choice.equals("bat"))
         {
             monster.setName("Bat");
         }
-        else if(temp==2)
+        else if(choice.equals("spider"))
         {
             monster.setName("Spider");
         }
-        else if(temp==3)
+        else if(choice.equals("slime"))
         {
             monster.setName("Slime");
         }
-        else if(temp==4)
+        else if(choice.equals("goblin"))
         {
             monster.setName("Goblin");
         }
-        else if(temp==5)
+        else if(choice.equals("wolf"))
         {
             monster.setName("Wolf");
         }
-        else if(temp==6)
+        else if(choice.equals("skeleton"))
         {
             monster.setName("Skeleton");
         }
-        else if(temp==7)
+        else if(choice.equals("zombie"))
         {
             monster.setName("Zombie");
         }
-        else if(temp==8)
+        else if(choice.equals("bugbear"))
         {
             monster.setName("Bugbear");
         }
-        else if(temp==9)
+        else if(choice.equals("troll"))
         {
             monster.setName("Troll");
         }
-        else if(temp==10)
+        else if(choice.equals("bandit"))
         {
             monster.setName("Bandit");
         }
-        else if(temp==11)
+        else if(choice.equals("nomad"))
         {
             monster.setName("Nomad");
         }
