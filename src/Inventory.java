71b16313d28a9c328938ff6480e7c001cc97da5d
kadensotek <bobby23jones@gmail.com>
2013-10-31 17:48:27 -0400

    Multiple items listed together; fixed bug in check for duplicates

--- src/Inventory.java
+++ src/Inventory.java
@@ -4,7 +4,7 @@ public class Inventory
 {
     private boolean empty;
     private ArrayList<Item> items = new ArrayList<Item>();
-    //private ArrayList<int> number = new ArrayList<int>();
+    private ArrayList<Integer> number = new ArrayList<Integer>();
 
     public Inventory()
     {
@@ -18,12 +18,29 @@ public class Inventory
 
     public void addItem(Item item)
     {
+        boolean duplicate = false;
+        int index = 0;
+
         if(isEmpty())
         {
             this.empty = false;
         }
 
-        this.items.add(item);
+        for(index=0; index<items.size(); index++)
+        {
+            if(item.getItemID() == this.items.get(index).getItemID())
+            {
+                this.number.set(index, this.number.get(index)+1);
+                duplicate = true;
+                break;
+            }
+        }
+
+        if(duplicate==false)
+        {
+            this.items.add(item);
+            this.number.add(1);
+        }
     }
 
     public void useItem(Item item)
@@ -38,7 +55,14 @@ public class Inventory
         {
             for(int i=0; i<items.size(); i++)
             {
-                System.out.printf("%d: %s\n", i+1, items.get(i).getName());
+                if(number.get(i)==1)
+                {
+                    System.out.printf("%d: %s\n", i+1, items.get(i).getName());
+                }
+                else
+                {
+                    System.out.printf("%d: %s x%d\n", i+1, items.get(i).getName(), number.get(i));
+                }
             }
         }
         else
